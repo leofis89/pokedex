7787e7e1d5c6fe1c114843a17413a2e106d643fc
leofis89 <jonyleonardo89@gmail.com>
2020-09-02 00:45:44 -0300

    chore: Add Layout - Home

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import '../styles/home.css';
+import Header from '../layouts/home';
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<div>
+			<Header />
+			<Component {...pageProps} />
+		</div>
+	);
 }
 
 export default MyApp;
